5aef05f3c62f9f869405426e163c1dadef4994f9
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2021-06-15 20:56:46 +0200

    projet

--- src/main/java/io/github/alexiscomete/lapinousecond/Main.java
+++ src/main/java/io/github/alexiscomete/lapinousecond/Main.java
@@ -1,7 +1,29 @@
 package io.github.alexiscomete.lapinousecond;
 
+
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+
+import java.io.IOException;
+
 public class Main {
+
+    public static DiscordApi api;
+    public static SaveLocation<String> config;
+
+    static {
+        try {
+            config = new SaveLocation<>(";", "/config.txt", (String a) -> a);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("RIP Lapinou premier");
+
+        api = new DiscordApiBuilder().setToken(config.getContent().get(0)).login().join();
+        api.updateActivity("Prefix : -");
+
     }
 }
