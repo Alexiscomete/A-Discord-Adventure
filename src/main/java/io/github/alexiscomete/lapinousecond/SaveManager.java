b3880323a886730b3484f582ea2d006cfe45b636
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2021-06-20 15:34:05 +0200

    juste un commit

--- src/main/java/io/github/alexiscomete/lapinousecond/SaveManager.java
+++ src/main/java/io/github/alexiscomete/lapinousecond/SaveManager.java
@@ -3,6 +3,11 @@ package io.github.alexiscomete.lapinousecond;
 import java.util.HashMap;
 
 public class SaveManager {
+
+    public static final String path = "zzz";
+    public static final String user = "root";
+    static final String mdp = "eqfq";
+
     public static HashMap<Long, Player> players = new HashMap<>();
     public static HashMap<Long, ServerBot> servers = new HashMap<>();
 
--- src/main/java/io/github/alexiscomete/lapinousecond/WorkEnum.java
+++ src/main/java/io/github/alexiscomete/lapinousecond/WorkEnum.java
@@ -2,13 +2,13 @@ package io.github.alexiscomete.lapinousecond;
 
 public enum WorkEnum {
 
-    WORK1("default", "Vous aider quelqu'un à trouver le salon 'memes', vous gagnez rc Rabbitcoins !", 5, 10),
-    WORK2("default", "Vous aider un bot à se rappeler de son préfix, vous gagnez rc Rabbitcoins !", 6, 15),
-    WORK3("default", "Vous trouvez billet de train pour un autre serveur que vous rendez à son propriétaire, il vous récompense avec rc Rabbitcoins ...", 3, 11),
-    WORK4("default", "Quelqu'un vous parle de Lapinou Premier, il vous donne rc Rabbitcoins car vous l'aider à le chercher, bien sûr personne ne le trouve.", 4, 7),
-    WORK5("default", "Le serveur vous récompense avec rc Rabbitcoins pour avoir défendu le serveur contre Alphabet Lapin !", 7, 12),
-    WORK6("default", "Carlos le chat virtuel est passé par là ... vous gagnez rc Rabbitcoins ...", 6, 9),
-    WORK7("default", "Vous passez un peu de temps avec les Togerts, ils vous donnent rc Rabbitcoins", 5, 12);
+    WORK1("default", "*Vous aider quelqu'un à trouver le salon 'memes', vous gagnez rc Rabbitcoins !*", 5, 10),
+    WORK2("default", "*Vous aider un bot à se rappeler de son préfix, vous gagnez rc Rabbitcoins !*", 6, 15),
+    WORK3("default", "*Vous trouvez billet de train pour un autre serveur que vous rendez à son propriétaire, il vous récompense avec rc Rabbitcoins ...*", 3, 11),
+    WORK4("default", "*Quelqu'un vous parle de Lapinou Premier, il vous donne rc Rabbitcoins car vous l'aider à le chercher, bien sûr personne ne le trouve.*", 4, 7),
+    WORK5("default", "*Le serveur vous récompense avec rc Rabbitcoins pour avoir défendu le serveur contre Alphabet Lapin !*", 7, 12),
+    WORK6("default", "*Carlos le chat virtuel est passé par là ... vous gagnez rc Rabbitcoins ...*", 6, 9),
+    WORK7("default", "*Vous passez un peu de temps avec les Togerts, ils vous donnent rc Rabbitcoins*", 5, 12);
 
     String type;
     String answer;
