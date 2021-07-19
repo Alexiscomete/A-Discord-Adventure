bc8c7800bad46af90e9a5048dc572d46d209624e
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2021-07-19 16:53:07 +0200

    Long[] to ArrayList

--- src/main/java/io/github/alexiscomete/lapinousecond/InventoryC.java
+++ src/main/java/io/github/alexiscomete/lapinousecond/InventoryC.java
@@ -13,19 +13,36 @@ public class InventoryC extends CommandBot {
 
     @Override
     void execute(MessageCreateEvent messageCreateEvent, String content, String[] args) {
-        Player p = SaveManager.getPlayer(messageCreateEvent.getMessageAuthor().getId());
-        if (p == null) {
-            messageCreateEvent.getMessage().reply("Vous devez d'abord faire la commande start avant de continuer");
+        if (args.length > 1) {
+            try {
+                Player p = SaveManager.getPlayer(Long.parseLong(args[1]));
+                if (p == null) {
+                    messageCreateEvent.getMessage().reply("Cette personne n'a pas encore de compte");
+                } else {
+                    invOf(p, messageCreateEvent);
+                }
+            } catch (NumberFormatException e) {
+                messageCreateEvent.getMessage().reply("Pour voir l'inventiare d'une personne, vous devez indiquez son id");
+            }
         } else {
-            EmbedBuilder builder = new EmbedBuilder();
-            builder.setDescription("Serveur actuel : " + p.getServer()).setTitle("Inventaire").setColor(Color.green);
-            builder.addField("Ressources et argent", "**Rabbitcoins :** "  + p.getBal());
-            messageCreateEvent.getMessage().reply(builder);
-            if (p.getTuto() == 2) {
-                messageCreateEvent.getMessage().reply("Bon ... comme vous l'avez vu vous n'avez pas d'argent (sauf si vous refaites le tuto 👀). Utilisez la commande `work` pour en gagner un peu ...");
-                p.setTuto((short) 3);
+            Player p = SaveManager.getPlayer(messageCreateEvent.getMessageAuthor().getId());
+            if (p == null) {
+                messageCreateEvent.getMessage().reply("Vous devez d'abord faire la commande start avant de continuer");
+            } else {
+                invOf(p, messageCreateEvent);
+                if (p.getTuto() == 2) {
+                    messageCreateEvent.getMessage().reply("Bon ... comme vous l'avez vu vous n'avez pas d'argent (sauf si vous refaites le tuto 👀). Utilisez la commande `work` pour en gagner un peu ...");
+                    p.setTuto((short) 3);
+                }
             }
         }
+    }
+
+    public void invOf(Player p, MessageCreateEvent messageCreateEvent) {
+        EmbedBuilder builder = new EmbedBuilder();
+        builder.setDescription("Serveur actuel : " + p.getServer()).setTitle("Inventaire").setColor(Color.green);
+        builder.addField("Ressources et argent", "**Rabbitcoins :** " + p.getBal());
+        messageCreateEvent.getMessage().reply(builder);
     }
 
 }
