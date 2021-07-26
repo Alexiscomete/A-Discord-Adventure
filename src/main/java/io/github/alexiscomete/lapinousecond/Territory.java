f25d92bbdc14c04ddf17b4d86b0079194a7ee0f8
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2021-07-26 16:30:47 +0200

    terr

---
+++ src/main/java/io/github/alexiscomete/lapinousecond/Territory.java
@@ -0,0 +1,70 @@
+package io.github.alexiscomete.lapinousecond;
+
+public class Territory {
+    public long getValue(long time, int numberOfBattles) {
+        return (long) getValueTime(time) * getValueBattles(numberOfBattles);
+    }
+
+    public int getValueTime(long time) {
+        long minutes = time / 60000;
+        if (minutes > 5) {
+            if (minutes > 15) {
+                if (minutes > 40) {
+                    if (minutes > 60) {
+                        if (minutes > 90) {
+                            if (minutes > 120) {
+                                if (minutes > 200) {
+                                    if (minutes > 260) {
+                                        if (minutes > 360) {
+                                            if (minutes > 460) {
+                                                if (minutes > 560) {
+                                                    if (minutes > 660) {
+                                                        if (minutes > 800) {
+                                                            if (minutes > 1000) {
+                                                                if (minutes > 2000) {
+                                                                    if (minutes > 3000) {
+                                                                        if (minutes > 4000) {
+                                                                            if (minutes > 5000) {
+                                                                                return (int) minutes;
+                                                                            }
+                                                                            return 1000;
+                                                                        }
+                                                                        return 600;
+                                                                    }
+                                                                    return 400;
+                                                                }
+                                                                return 200;
+                                                            }
+                                                            return 100;
+                                                        }
+                                                        return 80;
+                                                    }
+                                                    return 69;
+                                                }
+                                                return 56;
+                                            }
+                                            return 44;
+                                        }
+                                        return 35;
+                                    }
+                                    return 27;
+                                }
+                                return 22;
+                            }
+                            return 15;
+                        }
+                        return 10;
+                    }
+                    return 7;
+                }
+                return 5;
+            }
+            return 3;
+        }
+        return 1;
+    }
+
+    public int getValueBattles(int numberOfBattles) {
+        return 1;
+    }
+}
---
+++ src/main/java/io/github/alexiscomete/lapinousecond/WorldEnum.java
@@ -0,0 +1,4 @@
+package io.github.alexiscomete.lapinousecond;
+
+public enum WorldEnum {
+}
