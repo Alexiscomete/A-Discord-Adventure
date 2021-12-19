dc7daa54dbdd8e733d8e49dec19f3819cca22aca
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2021-12-19 19:06:19 +0100

    TEST

--- src/main/java/io/github/alexiscomete/lapinousecond/commands/classes/PermsManager.java
+++ src/main/java/io/github/alexiscomete/lapinousecond/commands/classes/PermsManager.java
@@ -7,13 +7,13 @@ import io.github.alexiscomete.lapinousecond.commands.CommandBot;
 import org.javacord.api.event.message.MessageCreateEvent;
 
 import java.sql.SQLException;
+import java.util.HashMap;
 
 public class PermsManager extends CommandBot {
     public PermsManager() {
         super("Commande permettant de modifier les permissions d'un utilisateur dans le bot", "pm", "pm [user] [perm name] [true/false]; perms : PLAY, CREATE_SERVER, SET_SERVER_SEC, MANAGE_PERMS", "MANAGE_PERMS");
     }
 
-    //TODO cette fonction est pourri !! il manque l'id
     @Override
     public void execute(MessageCreateEvent messageCreateEvent, String content, String[] args) {
         SaveManager saveManager = Main.getSaveManager();
@@ -26,15 +26,20 @@ public class PermsManager extends CommandBot {
         try {
             UserPerms userPerms = saveManager.getPlayerPerms(Long.parseLong(args[1]));
             if (userPerms.isDefault) {
-                SaveManager.st.executeUpdate("INSERT INTO perms (ID, PLAY, CREATE_SERVER, SET_SERVER_SEC, MANAGE_PERMS) VALUES (" + messageCreateEvent.getMessageAuthor().getId() + ", " + SaveManager.toBooleanString(userPerms.PLAY) + ", " + SaveManager.toBooleanString(userPerms.CREATE_SERVER) + ", " + SaveManager.toBooleanString(userPerms.SET_SERVER_SEC) + ", " + SaveManager.toBooleanString(userPerms.MANAGE_PERMS) + ")");
+                HashMap<String, String> what = new HashMap<>();
+                what.put("ID", String.valueOf(messageCreateEvent.getMessageAuthor().getId()));
+                what.put("PLAY", SaveManager.toBooleanString(userPerms.PLAY));
+                what.put("CREATE_SERVER", SaveManager.toBooleanString(userPerms.CREATE_SERVER));
+                what.put("SET_SERVER_SEC", SaveManager.toBooleanString(userPerms.SET_SERVER_SEC));
+                what.put("MANAGE_PERMS", SaveManager.toBooleanString(userPerms.MANAGE_PERMS));
+                saveManager.insert("perms", what);
             }
             if (perm.equalsIgnoreCase("MANAGE_PERMS") && !messageCreateEvent.getMessageAuthor().isBotOwner()) {
                 messageCreateEvent.getMessage().reply("Impossible .... vous devez être l'owner du bot pour modifier cette permission");
                 return;
             }
-            saveManager.setValue("perms", );
-            SaveManager.st.executeUpdate("UPDATE perms SET " + perm + " = " + SaveManager.toBooleanString(value));
-        } catch (NumberFormatException | SQLException e) {
+            saveManager.setValue("perms", messageCreateEvent.getMessageAuthor().getId(), perm, SaveManager.toBooleanString(value));
+        } catch (NumberFormatException e) {
             messageCreateEvent.getMessage().reply("Vous devez indiquer un format de nombre valide pour l'argument user");
         }
     }
