255e3aea7e5561ac9e746edfcf6560cc69af650d
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2021-12-19 17:05:35 +0100

    eee

--- src/main/java/io/github/alexiscomete/lapinousecond/commands/classes/PermsManager.java
+++ src/main/java/io/github/alexiscomete/lapinousecond/commands/classes/PermsManager.java
@@ -1,5 +1,6 @@
 package io.github.alexiscomete.lapinousecond.commands.classes;
 
+import io.github.alexiscomete.lapinousecond.Main;
 import io.github.alexiscomete.lapinousecond.SaveManager;
 import io.github.alexiscomete.lapinousecond.UserPerms;
 import io.github.alexiscomete.lapinousecond.commands.CommandBot;
@@ -12,8 +13,10 @@ public class PermsManager extends CommandBot {
         super("Commande permettant de modifier les permissions d'un utilisateur dans le bot", "pm", "pm [user] [perm name] [true/false]; perms : PLAY, CREATE_SERVER, SET_SERVER_SEC, MANAGE_PERMS", "MANAGE_PERMS");
     }
 
+    //TODO cette fonction est pourri !! il manque l'id
     @Override
-    void execute(MessageCreateEvent messageCreateEvent, String content, String[] args) {
+    public void execute(MessageCreateEvent messageCreateEvent, String content, String[] args) {
+        SaveManager saveManager = Main.getSaveManager();
         if (args.length < 4) {
             messageCreateEvent.getMessage().reply("pm [user] [perm name] [true/false]");
             return;
@@ -21,7 +24,7 @@ public class PermsManager extends CommandBot {
         boolean value = Boolean.parseBoolean(args[3]);
         String perm = args[2];
         try {
-            UserPerms userPerms = SaveManager.getPlayerPerms(Long.parseLong(args[1]));
+            UserPerms userPerms = saveManager.getPlayerPerms(Long.parseLong(args[1]));
             if (userPerms.isDefault) {
                 SaveManager.st.executeUpdate("INSERT INTO perms (PLAY, CREATE_SERVER, SET_SERVER_SEC, MANAGE_PERMS) VALUES (" + SaveManager.toBooleanString(userPerms.PLAY) + ", " + SaveManager.toBooleanString(userPerms.CREATE_SERVER) + ", " + SaveManager.toBooleanString(userPerms.SET_SERVER_SEC) + ", " + SaveManager.toBooleanString(userPerms.MANAGE_PERMS) + ")");
             }
@@ -29,6 +32,7 @@ public class PermsManager extends CommandBot {
                 messageCreateEvent.getMessage().reply("Impossible .... vous devez être l'owner du bot pour modifier cette permission");
                 return;
             }
+            saveManager.setValue("perms", );
             SaveManager.st.executeUpdate("UPDATE perms SET " + perm + " = " + SaveManager.toBooleanString(value));
         } catch (NumberFormatException | SQLException e) {
             messageCreateEvent.getMessage().reply("Vous devez indiquer un format de nombre valide pour l'argument user");
