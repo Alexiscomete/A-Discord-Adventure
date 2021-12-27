8f7ab426e6fe073157974e9999010ce881ccf34d
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2021-12-27 17:33:16 +0100

    convertir role <=> string

--- src/main/java/io/github/alexiscomete/lapinousecond/roles/RolesEnum.java
+++ src/main/java/io/github/alexiscomete/lapinousecond/roles/RolesEnum.java
@@ -1,23 +1,45 @@
 package io.github.alexiscomete.lapinousecond.roles;
 
-import java.util.function.Supplier;
+import java.util.ArrayList;
+import java.util.function.Function;
 
 public enum RolesEnum {
-    SERVER_OWNER(() -> new DefaultRole(100, 10000000, "Propriétaire de serveur", "Le rôle pour toute personne créant un serveur (il doit y avoir le bot dessus)")),
-    PROJECT_ADMIN(() -> new DefaultRole(100, 10000000, "Administrateur de projet", "Uniquement pour ceux qui sont admin sur un serveur de projet")),
-    GOUV(() -> new DefaultRole(200, 1000000, "Membre de l'assemblée", "Les membres de l'assemblée de la RPDB peuvent demander à avoir ce rôle")),
-    REPRESENTANT(() -> new DefaultRole(200, 10000000, "Représentant de région / département", "Chaque région peut définir ses conditions")),
-    VERIFIE(() -> new DefaultRole(10, 10000000, "Membre vérifié", "Vérifié par le bot de l'ORU")),
-    MODO(() -> new DefaultRole(100, 10000000, "Modérateur", "Pour toute personne ayant un rôle intermédiaire sur un serveur")),
-    PARTICIPANT(() -> new DefaultRole(100, 10000000, "Participant projet", "Pour toute personne participant à un projet (un peu plus qu'avec sa simple précense sur le serveur). A chaque projet de décider"));
+    SERVER_OWNER((id) -> new DefaultRole(100, 10000000, "Propriétaire de serveur", "Le rôle pour toute personne créant un serveur (il doit y avoir le bot dessus)", id, "SERVER_OWNER")),
+    PROJECT_ADMIN((id) -> new DefaultRole(100, 10000000, "Administrateur de projet", "Uniquement pour ceux qui sont admin sur un serveur de projet", id, "PROJECT_ADMIN")),
+    GOUV((id) -> new DefaultRole(200, 1000000, "Membre de l'assemblée", "Les membres de l'assemblée de la RPDB peuvent demander à avoir ce rôle", id, "GOUV")),
+    REPRESENTANT((id) -> new DefaultRole(200, 10000000, "Représentant de région / département", "Chaque région peut définir ses conditions", id, "REPRESENTANT")),
+    VERIFIE((id) -> new DefaultRole(10, 10000000, "Membre vérifié", "Vérifié par le bot de l'ORU", id, "VERIFIE")),
+    MODO((id) -> new DefaultRole(100, 10000000, "Modérateur", "Pour toute personne ayant un rôle intermédiaire sur un serveur", id, "MODO")),
+    PARTICIPANT((id) -> new DefaultRole(100, 10000000, "Participant projet", "Pour toute personne participant à un projet (un peu plus qu'avec sa simple précense sur le serveur). A chaque projet de décider", id, "PARTICIPANT"));
 
-    private final Supplier<Role> supplier;
+    private final Function<Long, Role> supplier;
 
-    public Role getInstance() {
-        return supplier.get();
+    public Role getInstance(Long serverID) {
+        return supplier.apply(serverID);
     }
 
-    RolesEnum(Supplier<Role> supplier) {
+    RolesEnum(Function<Long, Role> supplier) {
         this.supplier = supplier;
+    }
+
+    public static ArrayList<Role> getRoles(String rolesString) {
+        String[] strings = rolesString.split(";");
+        ArrayList<Role> roles = new ArrayList<>();
+        for (String str : strings) {
+            String[] serverAndRole = str.split(":");
+            roles.add(valueOf(serverAndRole[1]).getInstance(Long.parseLong(serverAndRole[0])));
+        }
+        return roles;
+    }
+
+    public static String rolesToString(ArrayList<Role> arrayList) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < arrayList.size(); i++) {
+            stringBuilder.append(arrayList.get(i).getServerID()).append(":").append(arrayList.get(i).getProgName());
+            if (i != arrayList.size()-1) {
+                stringBuilder.append(";");
+            }
+        }
+        return stringBuilder.toString();
     }
 }
