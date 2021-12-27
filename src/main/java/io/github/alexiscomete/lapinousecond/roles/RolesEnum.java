717ffacf2ab93e25c32cc89f1f0524aec744e14b
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2021-12-27 17:46:56 +0100

    roles suite : début check permission, ...

--- src/main/java/io/github/alexiscomete/lapinousecond/roles/RolesEnum.java
+++ src/main/java/io/github/alexiscomete/lapinousecond/roles/RolesEnum.java
@@ -6,6 +6,7 @@ import java.util.function.Function;
 public enum RolesEnum {
     SERVER_OWNER((id) -> new DefaultRole(100, 10000000, "Propriétaire de serveur", "Le rôle pour toute personne créant un serveur (il doit y avoir le bot dessus)", id, "SERVER_OWNER")),
     PROJECT_ADMIN((id) -> new DefaultRole(100, 10000000, "Administrateur de projet", "Uniquement pour ceux qui sont admin sur un serveur de projet", id, "PROJECT_ADMIN")),
+    SERVER_ADMIN((id) -> new DefaultRole(10, 1, "Administrateur de serveur", "Pour tout admin sur un serveur, à donner à toute personne qui soit gérer les permissions des rôles", id, "SERVER_ADMIN")),
     GOUV((id) -> new DefaultRole(200, 1000000, "Membre de l'assemblée", "Les membres de l'assemblée de la RPDB peuvent demander à avoir ce rôle", id, "GOUV")),
     REPRESENTANT((id) -> new DefaultRole(200, 10000000, "Représentant de région / département", "Chaque région peut définir ses conditions", id, "REPRESENTANT")),
     VERIFIE((id) -> new DefaultRole(10, 10000000, "Membre vérifié", "Vérifié par le bot de l'ORU", id, "VERIFIE")),
@@ -36,10 +37,43 @@ public enum RolesEnum {
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < arrayList.size(); i++) {
             stringBuilder.append(arrayList.get(i).getServerID()).append(":").append(arrayList.get(i).getProgName());
-            if (i != arrayList.size()-1) {
+            if (i != arrayList.size() - 1) {
                 stringBuilder.append(";");
             }
         }
         return stringBuilder.toString();
+    }
+
+    public static boolean check(String[] strings, Role[] roles, long serverID) {
+        for (String string : strings) {
+            boolean perm;
+            switch (string) {
+                case "MANAGE_ADMIN":
+                    perm = false;
+                    for (Role role : roles) {
+                        if (role.getServerID() == serverID && role.getProgName().equals("SERVER_OWNER")) {
+                            perm = true;
+                            break;
+                        }
+                    }
+                    if (!perm) {
+                        return false;
+                    }
+                    break;
+                case "MANAGE_ROLES_SERVER":
+                    perm = false;
+                    for (Role role : roles) {
+                        if (role.getServerID() == serverID && (role.getProgName().equals("SERVER_OWNER") || role.getProgName().equals("PROJECT_ADMIN") || role.getProgName().equals("SERVER_ADMIN"))) {
+                            perm = true;
+                            break;
+                        }
+                    }
+                    if (!perm) {
+                        return false;
+                    }
+                    break;
+            }
+        }
+        return true;
     }
 }
