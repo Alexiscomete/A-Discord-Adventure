ed4d611274ffe261e9c5010fbba7883d5cf5347e
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2021-12-28 20:11:32 +0100

    bug erreur --

--- src/main/java/io/github/alexiscomete/lapinousecond/roles/RolesEnum.java
+++ src/main/java/io/github/alexiscomete/lapinousecond/roles/RolesEnum.java
@@ -7,7 +7,7 @@ import java.util.function.Function;
 public enum RolesEnum {
     SERVER_OWNER((id) -> new DefaultRole(100, 10000000, "Propriétaire de serveur", "Le rôle pour toute personne créant un serveur (il doit y avoir le bot dessus)", id, "SERVER_OWNER")),
     PROJECT_ADMIN((id) -> new DefaultRole(100, 10000000, "Administrateur de projet", "Uniquement pour ceux qui sont admin sur un serveur de projet", id, "PROJECT_ADMIN")),
-    SERVER_ADMIN((id) -> new DefaultRole(10, 10000000, "Administrateur de serveur", "Pour tout admin sur un serveur, à donner à toute personne qui soit gérer les permissions des rôles", id, "SERVER_ADMIN")),
+    SERVER_ADMIN((id) -> new DefaultRole(10, 10000000, "Administrateur de serveur", "Pour tout admin sur un serveur, à donner à toute personne qui doit gérer les permissions des rôles", id, "SERVER_ADMIN")),
     GOUV((id) -> new DefaultRole(200, 1000000, "Membre de l'assemblé", "Les membres de l'assemblé de la RPDB peuvent demander à avoir ce rôle", id, "GOUV")),
     REPRESENTANT((id) -> new DefaultRole(200, 10000000, "Représentant de région / département", "Chaque région peut définir ses conditions", id, "REPRESENTANT")),
     VERIFIE((id) -> new DefaultRole(10, 10000000, "Membre vérifié", "Vérifié par le bot de l'ORU", id, "VERIFIE")),
@@ -25,6 +25,9 @@ public enum RolesEnum {
     }
 
     public static ArrayList<Role> getRoles(String rolesString) {
+        if (rolesString == null || rolesString.equals("")) {
+            return new ArrayList<>();
+        }
         String[] strings = rolesString.split(";");
         ArrayList<Role> roles = new ArrayList<>();
         for (String str : strings) {
