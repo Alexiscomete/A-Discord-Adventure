7cdead7bd03ee2b5df09ac96ab329e8ecd52516c
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2021-12-31 14:17:09 +0100

    place optional server

--- src/main/java/io/github/alexiscomete/lapinousecond/worlds/Place.java
+++ src/main/java/io/github/alexiscomete/lapinousecond/worlds/Place.java
@@ -1,4 +1,34 @@
 package io.github.alexiscomete.lapinousecond.worlds;
 
+import java.util.Optional;
+
 public class Place {
+    private Long serverID;
+    private ServerBot serverBot;
+
+
+    public Place(ServerBot serverBot) {
+        this.serverBot = serverBot;
+        if (serverBot == null) {
+            this.serverID = null;
+        } else {
+            this.serverID = serverBot.getId();
+        }
+    }
+
+    public Optional<ServerBot> getServerBot() {
+        return Optional.ofNullable(serverBot);
+    }
+
+    public Optional<Long> getServerID() {
+        return Optional.ofNullable(serverID);
+    }
+
+    public void setServerBot(ServerBot serverBot) {
+        this.serverBot = serverBot;
+    }
+
+    public void setServerID(Long serverID) {
+        this.serverID = serverID;
+    }
 }
