fc4a1523c5aa26b9ad9ec875f45d17b8bb769465
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-01-08 21:27:11 +0100

    suite

--- src/main/java/io/github/alexiscomete/lapinousecond/commands/classes/Help.java
+++ src/main/java/io/github/alexiscomete/lapinousecond/commands/classes/Help.java
@@ -2,11 +2,13 @@ package io.github.alexiscomete.lapinousecond.commands.classes;
 
 import io.github.alexiscomete.lapinousecond.ListenerMain;
 import io.github.alexiscomete.lapinousecond.commands.CommandBot;
+import org.javacord.api.entity.message.MessageBuilder;
+import org.javacord.api.entity.message.component.ActionRow;
+import org.javacord.api.entity.message.component.Button;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.event.message.MessageCreateEvent;
 
 import java.awt.*;
-import java.util.Collection;
 
 public class Help extends CommandBot {
 
@@ -18,9 +20,15 @@ public class Help extends CommandBot {
     public void execute(MessageCreateEvent messageCreateEvent, String content, String[] args) {
         System.out.println("help!!");
         EmbedBuilder builder = new EmbedBuilder();
+        MessageBuilder messageBuilder = new MessageBuilder();
+        messageBuilder.append(builder);
         builder.setDescription("Pensez au préfix !").setTitle("Aide").setColor(Color.blue);
         if (args.length < 2) {
             addCommands(builder, 0);
+            messageBuilder.addComponents(
+                    ActionRow.of(Button.success("success", "Send a message"),
+                            Button.danger("danger", "Delete this message"),
+                            Button.secondary("secondary", "Remind me after 5 minutes")));
         } else {
             CommandBot commandBot = ListenerMain.commands.get(args[1]);
             if (commandBot == null) {
@@ -29,8 +37,7 @@ public class Help extends CommandBot {
                 builder.addField(commandBot.getName(), commandBot.getTotalDescription());
             }
         }
-        messageCreateEvent.getMessage().reply(builder);
-
+        messageBuilder.replyTo(messageCreateEvent.getMessage());
     }
 
     public void addCommands(EmbedBuilder embedBuilder, int min) {
