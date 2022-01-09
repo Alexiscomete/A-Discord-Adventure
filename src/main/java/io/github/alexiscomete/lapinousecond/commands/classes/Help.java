fbefa842215df9165d00d65d716d7985f39058c2
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-01-09 15:08:53 +0100

    suite encore ... je fait finir par maitriser ça

--- src/main/java/io/github/alexiscomete/lapinousecond/commands/classes/Help.java
+++ src/main/java/io/github/alexiscomete/lapinousecond/commands/classes/Help.java
@@ -3,16 +3,17 @@ package io.github.alexiscomete.lapinousecond.commands.classes;
 import io.github.alexiscomete.lapinousecond.ListenerMain;
 import io.github.alexiscomete.lapinousecond.Main;
 import io.github.alexiscomete.lapinousecond.commands.CommandBot;
-import io.github.alexiscomete.lapinousecond.message_event.ButtonsManager;
 import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.message.MessageBuilder;
 import org.javacord.api.entity.message.component.ActionRow;
 import org.javacord.api.entity.message.component.Button;
+import org.javacord.api.entity.message.component.LowLevelComponent;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.event.interaction.MessageComponentCreateEvent;
 import org.javacord.api.event.message.MessageCreateEvent;
 
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.concurrent.ExecutionException;
 import java.util.function.Consumer;
@@ -25,42 +26,18 @@ public class Help extends CommandBot {
 
     @Override
     public void execute(MessageCreateEvent messageCreateEvent, String content, String[] args) {
-        System.out.println("help!!");
         EmbedBuilder builder = new EmbedBuilder();
-        MessageBuilder messageBuilder = new MessageBuilder();
         builder.setDescription("Pensez au préfix !").setTitle("Aide").setColor(Color.blue);
         if (args.length < 2) {
+            MessageBuilder messageBuilder = new MessageBuilder();
             addCommands(builder, 0);
+            EventAnswer eventAnswer = new EventAnswer(builder);
             messageBuilder.addComponents(
                     ActionRow.of(
                             Button.success("last_page", "Page précédente"),
                             Button.success("next_page", "Page suivante")
                     ));
-            try {
-                int[] level = {0};
-                messageBuilder.append(builder);
-                Message msg = messageBuilder.send(messageCreateEvent.getChannel()).get();
-                HashMap<String, Consumer<MessageComponentCreateEvent>> hashMap = new HashMap<>();
-                hashMap.put("next_page", messageComponentCreateEvent -> {
-                    if (level[0] + 10 < ListenerMain.commands.size()) {
-                        level[0] += 10;
-                        builder.removeAllFields();
-                        addCommands(builder, level[0]);
-                        messageComponentCreateEvent.getMessageComponentInteraction().createOriginalMessageUpdater().removeAllEmbeds().addEmbed(builder).update();
-                    }
-                });
-                hashMap.put("last_page", messageComponentCreateEvent -> {
-                    if (level[0] > 9) {
-                        level[0] -= 10;
-                        builder.removeAllFields();
-                        addCommands(builder, level[0]);
-                        messageComponentCreateEvent.getMessageComponentInteraction().createOriginalMessageUpdater().removeAllEmbeds().addEmbed(builder).update();
-                    }
-                });
-                Main.getButtonsManager().addMessage(msg.getId(), hashMap);
-            } catch (InterruptedException | ExecutionException e) {
-                e.printStackTrace();
-            }
+            messageBuilder.append(builder);
         } else {
             CommandBot commandBot = ListenerMain.commands.get(args[1]);
             if (commandBot == null) {
@@ -68,7 +45,7 @@ public class Help extends CommandBot {
             } else {
                 builder.addField(commandBot.getName(), commandBot.getTotalDescription());
             }
-            messageBuilder.replyTo(messageCreateEvent.getMessage());
+            messageCreateEvent.getMessage().reply(builder);
         }
     }
 
@@ -81,6 +58,57 @@ public class Help extends CommandBot {
             }
         } else {
             embedBuilder.addField("Erreur", "Impossible de trouver la commande n°" + min);
+        }
+    }
+
+    public class EventAnswer {
+
+        private int level = 0;
+        private final EmbedBuilder builder;
+
+        public void next(MessageComponentCreateEvent messageComponentCreateEvent) {
+            if (level + 10 < ListenerMain.commands.size()) {
+                level += 10;
+                builder.removeAllFields();
+                addCommands(builder, level);
+                messageComponentCreateEvent.getMessageComponentInteraction().createOriginalMessageUpdater().removeAllEmbeds().addEmbed(builder).update();
+            }
+        }
+
+        public void last(MessageComponentCreateEvent messageComponentCreateEvent) {
+            if (level > 9) {
+                level -= 10;
+                builder.removeAllFields();
+                addCommands(builder, level);
+                messageComponentCreateEvent.getMessageComponentInteraction().createOriginalMessageUpdater().removeAllEmbeds().addEmbed(builder).update();
+            }
+        }
+
+        public ActionRow getComponents() {
+            if (level > 0 && level + 10 < ListenerMain.commands.size()) {
+                return ActionRow.of(
+                        Button.success("last_page", "Page précédente"),
+                        Button.success("next_page", "Page suivante")
+                );
+            } else if (level > 0) {
+                return ActionRow.of(
+                        Button.success("last_page", "Page précédente")
+                );
+            } else if (level + 10 < ListenerMain.commands.size()) {
+                return ActionRow.of(
+                        Button.success("next_page", "Page suivante")
+                );
+            } else {
+                return ActionRow.of();
+            }
+        }
+
+        public EventAnswer(EmbedBuilder embedBuilder, long id) {
+            builder = embedBuilder;
+            HashMap<String, Consumer<MessageComponentCreateEvent>> hashMap = new HashMap<>();
+            hashMap.put("next_page", this::next);
+            hashMap.put("last_page", this::last);
+            Main.getButtonsManager().addMessage(id, hashMap);
         }
     }
 }
