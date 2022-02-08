9d17bf0a58bb70435d32ac081cf9d9e0a14350df
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-02-08 19:37:45 +0100

    eee

--- src/main/java/io/github/alexiscomete/lapinousecond/worlds/buildings/BuildProject.java
+++ src/main/java/io/github/alexiscomete/lapinousecond/worlds/buildings/BuildProject.java
@@ -1,9 +1,14 @@
 package io.github.alexiscomete.lapinousecond.worlds.buildings;
 
+import io.github.alexiscomete.lapinousecond.Main;
 import io.github.alexiscomete.lapinousecond.save.SaveLocation;
+import io.github.alexiscomete.lapinousecond.save.Tables;
 import io.github.alexiscomete.lapinousecond.useful.ProgressionBar;
 import org.javacord.api.entity.message.MessageBuilder;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
+
+import java.util.AbstractMap;
+import java.util.HashMap;
 
 public class BuildProject extends Building {
 
@@ -11,7 +16,7 @@ public class BuildProject extends Building {
 
     public BuildProject(long id) {
         super(id);
-        progressionBar = new ProgressionBar("💰", 3, "🧱", 3, " ", 1, 1, 0, 3);
+        progressionBar = new ProgressionBar("💰", 3, "🧱", 3, " ", 1, Double.parseDouble(getString("collect_target")), Double.parseDouble(getString("collect_value")), 60);
     }
 
     @Override
@@ -24,15 +29,21 @@ public class BuildProject extends Building {
         return null;
     }
 
-    public BuildProject(Buildings buildings) {
+    @SafeVarargs
+    public BuildProject(Buildings buildings, double price, String ownerType, String owner, AbstractMap.SimpleEntry<String, String>... specialInfos) {
         super(SaveLocation.generateUniqueID());
-        createNew(buildings);
-    }
-
-    public void createNew(Buildings buildings) {
-        set("id", String.valueOf(id));
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("id", String.valueOf(id));
+        Main.getSaveManager().insert(Tables.BUILDINGS.getTable().getName(), hashMap);
+        set("collect_target", String.valueOf(price));
         set("type", buildings.getType());
+        set("build_status", "building");
+        set("owner_type", ownerType);
+        set("owner", owner);
+        for (AbstractMap.SimpleEntry<String, String> special :
+             specialInfos) {
+            set(special.getKey(), special.getValue());
+        }
+        progressionBar = new ProgressionBar("💰", 3, "🧱", 3, " ", 1, price, 0.0, 60);
     }
-
-
 }
