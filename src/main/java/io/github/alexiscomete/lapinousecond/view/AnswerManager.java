83d4d31aae30273abc54a9c3136638c63c1767ae
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-02-12 21:34:33 +0100

    début langage (et respect du model MVC)

--- src/main/java/io/github/alexiscomete/lapinousecond/view/AnswerManager.java
+++ src/main/java/io/github/alexiscomete/lapinousecond/view/AnswerManager.java
@@ -1,4 +1,20 @@
 package io.github.alexiscomete.lapinousecond.view;
 
+import org.json.JSONObject;
+
+import java.util.Scanner;
+
 public class AnswerManager {
+    private JSONObject jsonObject;
+
+    public AnswerManager(String path) {
+        Scanner sc = new Scanner(path);
+        StringBuilder builder = new StringBuilder();
+        sc.forEachRemaining(builder::append);
+        jsonObject = new JSONObject(builder.toString());
+    }
+
+    public String getAnswer(LangageEnum langageEnum, AnswerEnum answerEnum) {
+        return null;
+    }
 }
---
+++ src/main/java/io/github/alexiscomete/lapinousecond/view/LangageEnum.java
@@ -0,0 +1,4 @@
+package io.github.alexiscomete.lapinousecond.view;
+
+public enum LangageEnum {
+}
