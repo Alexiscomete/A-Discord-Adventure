618edf57f3105f23468898f64bfe467bde15c0fb
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-02-18 11:52:49 +0100

    suite tr

--- src/main/java/io/github/alexiscomete/lapinousecond/useful/FullTransaction.java
+++ src/main/java/io/github/alexiscomete/lapinousecond/useful/FullTransaction.java
@@ -1,10 +1,13 @@
 package io.github.alexiscomete.lapinousecond.useful;
 
+import io.github.alexiscomete.lapinousecond.entity.Player;
+import org.javacord.api.entity.channel.TextChannel;
+
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class FullTransaction extends VerifTransaction {
-    public FullTransaction(Consumer<Double> addMoney, Consumer<Double> removeMoney, Supplier<Double> getMoney) {
-        super(addMoney, removeMoney, getMoney);
+    public FullTransaction(Consumer<Double> addMoney, Consumer<Double> removeMoney, Supplier<Double> getMoney, Player p, TextChannel textChannel) {
+        super(addMoney, removeMoney, getMoney, p, textChannel);
     }
 }
--- src/main/java/io/github/alexiscomete/lapinousecond/useful/VerifTransaction.java
+++ src/main/java/io/github/alexiscomete/lapinousecond/useful/VerifTransaction.java
@@ -1,11 +1,24 @@
 package io.github.alexiscomete.lapinousecond.useful;
 
+import io.github.alexiscomete.lapinousecond.entity.Player;
+import org.javacord.api.entity.channel.TextChannel;
+import org.javacord.api.entity.message.MessageBuilder;
+
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class VerifTransaction extends Transaction {
-    
-    public VerifTransaction(Consumer<Double> addMoney, Consumer<Double> removeMoney, Supplier<Double> getMoney) {
+
+    Player p;
+    TextChannel textChannel;
+
+    public VerifTransaction(Consumer<Double> addMoney, Consumer<Double> removeMoney, Supplier<Double> getMoney, Player p, TextChannel textChannel) {
         super(addMoney, removeMoney, getMoney);
+        this.p = p;
+        this.textChannel = textChannel;
+    }
+
+    public void ask(Double quantity) {
+        MessageBuilder messageBuilder = new MessageBuilder();
     }
 }
