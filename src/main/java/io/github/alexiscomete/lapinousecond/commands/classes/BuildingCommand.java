c88dd82257d7679d85d71253a32d9d00b83cd0d2
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-02-21 19:22:44 +0100

    build c

--- src/main/java/io/github/alexiscomete/lapinousecond/commands/classes/BuildingCommand.java
+++ src/main/java/io/github/alexiscomete/lapinousecond/commands/classes/BuildingCommand.java
@@ -6,12 +6,20 @@ import org.javacord.api.event.message.MessageCreateEvent;
 
 public class BuildingCommand extends CommandInServer {
 
-    public BuildingCommand(String description, String name, String totalDescription, String... perms) {
-        super(description, name, totalDescription, perms);
+    public BuildingCommand() {
+        super("Toutes les actions relatives aux bâtiments", "build", "Utilisation :\n- enter [id] pour entrer dans un bâtiment\n- exit pour sortir\n- la commande seule permet de voir la liste des bâtiments ou de voir les informations rapides\n- infos pour voir les infos détaillées");
     }
 
     @Override
     public void executeC(MessageCreateEvent messageCreateEvent, String content, String[] args, Player p) {
+        String building = p.getString("building");
+        if (building.equals("") || building.equals("exit"))
+        if (args.length > 1) {
+            switch (args[1]) {
 
+            }
+        } else {
+
+        }
     }
 }
