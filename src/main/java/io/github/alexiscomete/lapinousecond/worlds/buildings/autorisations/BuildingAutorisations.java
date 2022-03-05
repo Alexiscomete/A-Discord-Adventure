c35a86dfc295ae1d1dc0e9470c54f4bd8547117f
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-03-05 11:34:10 +0100

    suite

--- src/main/java/io/github/alexiscomete/lapinousecond/worlds/buildings/autorisations/BuildingAutorisations.java
+++ src/main/java/io/github/alexiscomete/lapinousecond/worlds/buildings/autorisations/BuildingAutorisations.java
@@ -1,4 +1,18 @@
 package io.github.alexiscomete.lapinousecond.worlds.buildings.autorisations;
 
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+
 public class BuildingAutorisations {
+    private final ArrayList<BuildingAutorisation> buildingAutorisationArrayList;
+
+    public BuildingAutorisations(ArrayList<BuildingAutorisation> buildingAutorisationArrayList) {
+        this.buildingAutorisationArrayList = buildingAutorisationArrayList;
+    }
+
+    public BuildingAutorisations(JSONArray jsonArray) {
+        ArrayList<BuildingAutorisation> buildingAutorisations = new ArrayList<>();
+        this.buildingAutorisationArrayList = buildingAutorisations;
+    }
 }
