135e17dc0104eebef5f5319460eec66b3d777113
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-03-17 19:09:47 +0100

    extends

--- src/main/java/io/github/alexiscomete/lapinousecond/worlds/buildings/interactions/Journal.java
+++ src/main/java/io/github/alexiscomete/lapinousecond/worlds/buildings/interactions/Journal.java
@@ -1,4 +1,28 @@
 package io.github.alexiscomete.lapinousecond.worlds.buildings.interactions;
 
-public class Journal {
+import io.github.alexiscomete.lapinousecond.entity.Player;
+import io.github.alexiscomete.lapinousecond.worlds.buildings.Building;
+import io.github.alexiscomete.lapinousecond.worlds.buildings.BuildingInteraction;
+import org.javacord.api.entity.message.MessageBuilder;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
+
+public class Journal extends BuildingInteraction {
+    public Journal(Building building) {
+        super(building);
+    }
+
+    @Override
+    public EmbedBuilder getInfos(Player p) {
+        return null;
+    }
+
+    @Override
+    public MessageBuilder getCompleteInfos(Player p) {
+        return null;
+    }
+
+    @Override
+    public void configBuilding() {
+
+    }
 }
