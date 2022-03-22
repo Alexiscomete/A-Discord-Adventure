79bfb5ddd1b77e3885f6f76828d63ce066d6d7d9
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-03-22 17:18:05 +0100

    e

--- src/main/java/io/github/alexiscomete/lapinousecond/view/AnswerManager.java
+++ src/main/java/io/github/alexiscomete/lapinousecond/view/AnswerManager.java
@@ -1,7 +1,6 @@
 package io.github.alexiscomete.lapinousecond.view;
 
 import org.json.JSONObject;
-
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
