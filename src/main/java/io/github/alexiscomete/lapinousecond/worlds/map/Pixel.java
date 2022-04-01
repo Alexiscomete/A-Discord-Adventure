2147ec237c9c9ba31988566f58b0a17850408ad0
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-04-01 18:27:55 +0200

    pixel

---
+++ src/main/java/io/github/alexiscomete/lapinousecond/worlds/map/Pixel.java
@@ -0,0 +1,27 @@
+package io.github.alexiscomete.lapinousecond.worlds.map;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class Pixel {
+    private final int x, xImage, y, yImage;
+    private final int xMax, yMax;
+    private final Color color;
+    private final boolean isDirt;
+
+    public Pixel(int x, int y, int xMax, int yMax, BufferedImage image) {
+        this.x = x;
+        this.y = y;
+        this.yMax = yMax;
+        this.xMax = xMax;
+        this.xImage = x * image.getWidth(null) / xMax;
+        this.yImage = y * image.getHeight(null) / yMax;
+        // permet de récupérer la couleur du pixel sur l'image
+        this.color = new Color(image.getRGB(xImage, yImage));
+        if (color.getBlue() > color.getRed() + color.getGreen()) {
+            this.isDirt = false;
+        } else {
+            this.isDirt = true;
+        }
+    }
+}
