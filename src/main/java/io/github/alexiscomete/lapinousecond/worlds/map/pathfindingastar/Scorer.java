8f54ac2c29ced7ec9aee39f904a83771e721d7e6
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-04-17 15:19:11 +0200

    le truc d'internet était nul

--- src/main/java/io/github/alexiscomete/lapinousecond/worlds/map/pathfindingastar/Scorer.java
+++
@@ -1,7 +0,0 @@
-package io.github.alexiscomete.lapinousecond.worlds.map.pathfindingastar;
-
-import io.github.alexiscomete.lapinousecond.worlds.map.Pixel;
-
-public interface Scorer {
-    double computeCost(Pixel from, Pixel to);
-}
