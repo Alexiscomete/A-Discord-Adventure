a8514a7a9861655316688159517720497e7078d7
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-04-28 13:31:38 +0200

    suite test

--- src/main/java/io/github/alexiscomete/lapinousecond/worlds/PlaceForTest.java
+++ src/main/java/io/github/alexiscomete/lapinousecond/worlds/PlaceForTest.java
@@ -2,4 +2,33 @@ package io.github.alexiscomete.lapinousecond.worlds;
 
 public class PlaceForTest extends Place{
 
+    String name;
+    int x, y;
+    long id;
+
+    public PlaceForTest(String name, long id, int x, int y) {
+        this.name = name;
+        this.id = id;
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public long getId() {
+        return id;
+    }
+
+    @Override
+    public String getString(String key) {
+        switch (key) {
+            case "name":
+                return name;
+            case "x":
+                return String.valueOf(x);
+            case "y":
+                return String.valueOf(y);
+            default:
+                return "";
+        }
+    }
 }
