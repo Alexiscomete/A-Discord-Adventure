d37d9a83c7cba303e8468a4e30dc43a20a0a2f7d
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-04-28 13:43:21 +0200

    suite test

--- src/main/java/io/github/alexiscomete/lapinousecond/worlds/PlaceForTest.java
+++ src/main/java/io/github/alexiscomete/lapinousecond/worlds/PlaceForTest.java
@@ -1,5 +1,9 @@
 package io.github.alexiscomete.lapinousecond.worlds;
 
+import io.github.alexiscomete.lapinousecond.worlds.map.Map;
+
+import java.util.ArrayList;
+
 public class PlaceForTest extends Place{
 
     String name;
@@ -31,4 +35,34 @@ public class PlaceForTest extends Place{
                 return "";
         }
     }
+
+    // génère aléatoirement une place pour les tests, minx = 0, maxx = voir dans Map.java, miny = 0, maxy = voir dans Map.java
+    public static PlaceForTest generateRandomPlace() {
+
+        // génération des coordonnées aléatoires
+        int x = (int) (Math.random() * Map.MAP_HEIGHT);
+        int y = (int) (Math.random() * Map.MAP_HEIGHT);
+
+        // génération du nom aléatoire
+        StringBuilder name = new StringBuilder();
+        int nb = (int) (Math.random() * 10);
+        for (int i = 0; i < nb; i++) {
+            name.append((char) (Math.random() * 26 + 'a'));
+        }
+
+        // génération de l'id aléatoire
+        long id = (long) (Math.random() * Long.MAX_VALUE);
+
+        return new PlaceForTest(name.toString(), id, x, y);
+    }
+
+    // génération de n places aléatoires
+    public static ArrayList<PlaceForTest> generateRandomPlaces(int n) {
+        ArrayList<PlaceForTest> places = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            places.add(generateRandomPlace());
+        }
+        return places;
+    }
+
 }
