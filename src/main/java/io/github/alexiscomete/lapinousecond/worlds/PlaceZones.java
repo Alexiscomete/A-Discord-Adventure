d3412a82a4c15b81a9be94c96fc9f972701155fc
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-05-05 17:18:48 +0200

    suite

---
+++ src/main/java/io/github/alexiscomete/lapinousecond/worlds/PlaceZones.java
@@ -0,0 +1,15 @@
+package io.github.alexiscomete.lapinousecond.worlds;
+
+import java.util.ArrayList;
+
+public class PlaceZones extends Place {
+    ArrayList<Zone> zones = new ArrayList<>();
+
+    public PlaceZones() {
+        super();
+    }
+
+    public PlaceZones(long id) {
+        super(id);
+    }
+}
