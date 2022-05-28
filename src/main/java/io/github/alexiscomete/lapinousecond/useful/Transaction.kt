381a502ef571d2c01491d98b9855db41564ac600
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-05-28 21:05:48 +0200

    kotlin

--- src/main/java/io/github/alexiscomete/lapinousecond/useful/Transaction.kt
+++ src/main/java/io/github/alexiscomete/lapinousecond/useful/Transaction.kt
@@ -4,43 +4,37 @@ import io.github.alexiscomete.lapinousecond.entity.Player
 import io.github.alexiscomete.lapinousecond.view.AnswerEnum
 import org.javacord.api.entity.channel.TextChannel
 import java.util.function.Consumer
-import java.util.function.Supplier
 
 open class Transaction(
-    private val addMoney: Consumer<Double>,
-    private val removeMoney: Consumer<Double>,
-    private val getMoney: Supplier<Double>
+    private val addMoney: (Double) -> Unit,
+    private val removeMoney: (Double) -> Unit,
+    private val getMoney: () -> Double
 ) {
     fun make(textChannel: TextChannel, quantity: Double, player: Player) {
-        make(
-            quantity,
-            player,
-            { content: String? -> textChannel.sendMessage(content) }) { content: String? ->
+        make(quantity, player, { content: String? -> textChannel.sendMessage(content) }) { content: String? ->
             textChannel.sendMessage(
                 content
             )
         }
     }
 
-    fun make(quantity: Double, player: Player, acc: Consumer<String?>, de: Consumer<String?>) {
+    fun make(quantity: Double, player: Player, acc: (String) -> Unit, de: (String) -> Unit) {
         if (basic(quantity)) {
-            acc.accept(player.getAnswer(AnswerEnum.TR_END, true))
+            acc(player.getAnswer(AnswerEnum.TR_END, true))
         } else {
-            de.accept(
+            de(
                 player.getAnswer(
-                    AnswerEnum.ECHEC_TRANS,
-                    true,
-                    player.getAnswer(AnswerEnum.NO_ENOUGH_MONEY, false)
+                    AnswerEnum.ECHEC_TRANS, true, player.getAnswer(AnswerEnum.NO_ENOUGH_MONEY, false)
                 )
             )
         }
     }
 
     private fun basic(quantity: Double): Boolean {
-        val money = getMoney.get()
+        val money = getMoney()
         return if (money > quantity) {
-            addMoney.accept(quantity)
-            removeMoney.accept(quantity)
+            addMoney(quantity)
+            removeMoney(quantity)
             true
         } else {
             false
