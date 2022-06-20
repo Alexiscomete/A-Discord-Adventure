00fdef24fbaf17b31da3fe7ffb3db345fe866599
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-06-20 09:56:21 +0200

    suite transaction

--- src/main/java/io/github/alexiscomete/lapinousecond/MessageException.kt
+++ src/main/java/io/github/alexiscomete/lapinousecond/MessageException.kt
@@ -1,4 +1,11 @@
 package io.github.alexiscomete.lapinousecond
 
+import io.github.alexiscomete.lapinousecond.entity.Player
+import io.github.alexiscomete.lapinousecond.view.AnswerEnum
+
 class MessageException(message: String) : Exception(message) {
+
+    fun throwMessageWithPlayerLanguage(player: Player) {
+
+    }
 }
--- src/main/java/io/github/alexiscomete/lapinousecond/commands/classes/Give.kt
+++
@@ -1,8 +0,0 @@
-package io.github.alexiscomete.lapinousecond.commands.classes
-
-import io.github.alexiscomete.lapinousecond.commands.CommandBot
-import org.javacord.api.event.message.MessageCreateEvent
-
-class Give : CommandBot("description", "give", "totalDescription") {
-    override fun execute(messageCreateEvent: MessageCreateEvent, content: String, args: Array<String>) {}
-}
