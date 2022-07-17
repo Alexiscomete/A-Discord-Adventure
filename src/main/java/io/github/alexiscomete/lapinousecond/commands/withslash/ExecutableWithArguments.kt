264f389df66f260113a46b483da5a5c3552b62d6
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-07-17 18:39:01 +0200

    all commands

--- src/main/java/io/github/alexiscomete/lapinousecond/commands/withslash/ExecutableWithArguments.kt
+++ src/main/java/io/github/alexiscomete/lapinousecond/commands/withslash/ExecutableWithArguments.kt
@@ -1,19 +1,22 @@
 package io.github.alexiscomete.lapinousecond.commands.withslash
 
+import io.github.alexiscomete.lapinousecond.entity.Player
+import io.github.alexiscomete.lapinousecond.entity.players
 import org.javacord.api.interaction.SlashCommandInteraction
-import org.javacord.api.interaction.SlashCommandInteractionOption
 
 interface ExecutableWithArguments {
     fun execute(slashCommand: SlashCommandInteraction)
 
-    fun getServer(slashCommand: SlashCommandInteraction) {
-
-    }
-    fun getServerBot(slashCommand: SlashCommandInteraction) {
+    fun getCurrentServerBot(slashCommand: SlashCommandInteraction) {
 
     }
 
-    fun getAccount(slashCommandInteraction: SlashCommandInteraction) {
-
+    fun getAccount(slashCommandInteraction: SlashCommandInteraction): Player {
+        try {
+            return players[slashCommandInteraction.user.id]
+                ?: throw IllegalStateException("Vous devez avoir un compte pour utiliser cette commande. Utilisez -start")
+        } catch (e: Exception) {
+            throw IllegalStateException("Vous devez avoir un compte pour utiliser cette commande. Utilisez -start")
+        }
     }
 }
