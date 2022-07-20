654ad879c0484390c6dfcd93a52e03090fceb542
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-07-20 16:56:10 +0200

    suite des commandes

--- src/main/java/io/github/alexiscomete/lapinousecond/commands/withslash/Command.kt
+++ src/main/java/io/github/alexiscomete/lapinousecond/commands/withslash/Command.kt
@@ -1,21 +1,55 @@
 package io.github.alexiscomete.lapinousecond.commands.withslash
 
+import io.github.alexiscomete.lapinousecond.api
 import org.javacord.api.entity.permission.PermissionType
 import org.javacord.api.interaction.SlashCommand
+import org.javacord.api.interaction.SlashCommandOption
+import java.util.EnumSet
 
 class Command(
     val name: String,
     val description: String,
     val usage: String,
-    val discordPerms: List<PermissionType>? = null,
-    val inDms: Boolean = true,
-    val botPerms: List<String>? = null,
-    val subCommands: List<SubCommand>? = null,
-    val subCommandsGroups: List<SubCommandGroup>? = null
+    discordPerms: EnumSet<PermissionType>? = null,
+    inDms: Boolean = true,
+    subCommands: List<Sub>? = null,
+    val botPerms: List<String>? = null
 ) {
     init {
         if (discordPerms == null) {
-            SlashCommand.with(name, description)
+            if (subCommands == null) {
+                SlashCommand.with(name, description)
+                    .setEnabledInDms(inDms)
+                    .createGlobal(api)
+                    .join()
+            } else {
+                val options = ArrayList<SlashCommandOption>()
+                for (sub in subCommands) {
+                    options.add(sub.getS())
+                }
+                SlashCommand.with(name, description, options)
+                    .setEnabledInDms(inDms)
+                    .createGlobal(api)
+                    .join()
+            }
+        } else {
+            if (subCommands == null) {
+                SlashCommand.with(name, description)
+                    .setEnabledInDms(inDms)
+                    .setDefaultEnabledForPermissions(discordPerms)
+                    .createGlobal(api)
+                    .join()
+            } else {
+                val options = ArrayList<SlashCommandOption>()
+                for (sub in subCommands) {
+                    options.add(sub.getS())
+                }
+                SlashCommand.with(name, description, options)
+                    .setEnabledInDms(inDms)
+                    .setDefaultEnabledForPermissions(discordPerms)
+                    .createGlobal(api)
+                    .join()
+            }
         }
     }
 }
