bc05129515d631e76942ff19fe86951f9a720bef
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-07-23 12:24:05 +0200

    suite shop

--- src/main/java/io/github/alexiscomete/lapinousecond/commands/withslash/classes/ShopCommand.kt
+++ src/main/java/io/github/alexiscomete/lapinousecond/commands/withslash/classes/ShopCommand.kt
@@ -3,8 +3,10 @@ package io.github.alexiscomete.lapinousecond.commands.withslash.classes
 import io.github.alexiscomete.lapinousecond.commands.withslash.Command
 import io.github.alexiscomete.lapinousecond.commands.withslash.ExecutableWithArguments
 import io.github.alexiscomete.lapinousecond.commands.withslash.SubCommand
+import io.github.alexiscomete.lapinousecond.resources.Resource
 import org.javacord.api.interaction.SlashCommandInteraction
 import org.javacord.api.interaction.SlashCommandOption
+import org.javacord.api.interaction.SlashCommandOptionChoice
 import org.javacord.api.interaction.SlashCommandOptionType
 
 class ShopCommandBase : Command(
@@ -25,9 +27,20 @@ class ShopBuyCommand :
         "buy",
         "Acheter des items / resources pour un mauvais prix",
         arrayListOf(
-            SlashCommandOption.createWithChoices(SlashCommandOptionType.STRING, "name", "Nom de l'item à acheter", true, arrayListOf(
-
-            )),
+            SlashCommandOption.createWithChoices(
+                SlashCommandOptionType.STRING,
+                "name",
+                "Nom de l'item à acheter",
+                true,
+                run {
+                    val arrayList = arrayListOf<SlashCommandOptionChoice>()
+                    Resource.values().forEach {
+                        arrayList.add(SlashCommandOptionChoice.create(it.name, it.progName))
+                    }
+                    return@run arrayList
+                }
+            ),
+            SlashCommandOption.createDecimalOption("quantity", "Quantité à acheter", false, 1.0, Double.MAX_VALUE)
         )
     ),
     ExecutableWithArguments {
