21f39fb5c9774fc39745ca8a214b8cc8d542641f
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-08-06 17:52:14 +0300

    Update Sub.kt

--- src/main/java/io/github/alexiscomete/lapinousecond/commands/withslash/Sub.kt
+++ src/main/java/io/github/alexiscomete/lapinousecond/commands/withslash/Sub.kt
@@ -2,9 +2,21 @@ package io.github.alexiscomete.lapinousecond.commands.withslash
 
 import org.javacord.api.interaction.SlashCommandOption
 
+/**
+ * contenue dans une commande, utilisée dans les classes abstraites de groupes et de sous commandes uniquement  
+*/
 interface Sub {
+    /**
+     * Le nom de cette sous commande, utilisée uniquement côté Discord
+    */   
     val name: String
+    /**
+     * de même que le nom, utilisé pour envoyer à Discord 
+    */
     val description: String
 
+    /**
+     * obtenir les informations complètes à envoyer à Discord , nom et description utilisés ici.
+    */
     fun getS(): SlashCommandOption
 }
