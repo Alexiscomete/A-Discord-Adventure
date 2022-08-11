a8d878ead9a0cad97595e94c1ba21a557352b6b1
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-08-11 20:59:49 +0200

    Update ResourceManager.kt

--- src/main/java/io/github/alexiscomete/lapinousecond/resources/ResourceManager.kt
+++ src/main/java/io/github/alexiscomete/lapinousecond/resources/ResourceManager.kt
@@ -3,16 +3,20 @@ package io.github.alexiscomete.lapinousecond.resources
 class ResourceManager(val resource: Resource, var quantity: Int) {
 
     override fun toString(): String {
+        // pour sauvegarder dans la base de données
         return resource.progName + ":" + quantity
     }
 
     companion object {
         fun stringToArray(str: String?): HashMap<Resource, ResourceManager> {
+            // la futur liste de toutes les ressources du joueur
             val resourceManagers = HashMap<Resource, ResourceManager>()
             if (str == null || str == "") {
                 return resourceManagers
             }
+            // pn sépare chaque ressource
             val strings = str.split(" ".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+            // comme vu précédemment le nom est suivi de : et de la quabtité
             for (s in strings) {
                 val elements = s.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
                 if (elements.size > 1) {
