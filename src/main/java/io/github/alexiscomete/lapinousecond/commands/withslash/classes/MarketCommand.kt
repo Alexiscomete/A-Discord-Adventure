ed2e97e5818fd9d8061b89241d8d94a1d47eadb7
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-08-13 19:30:20 +0200

    je doit arrêter

--- src/main/java/io/github/alexiscomete/lapinousecond/commands/withslash/classes/MarketCommand.kt
+++ src/main/java/io/github/alexiscomete/lapinousecond/commands/withslash/classes/MarketCommand.kt
@@ -1,4 +1,22 @@
 package io.github.alexiscomete.lapinousecond.commands.withslash.classes
 
-class MarketCommand {
+import io.github.alexiscomete.lapinousecond.commands.withslash.Command
+import io.github.alexiscomete.lapinousecond.commands.withslash.ExecutableWithArguments
+import org.javacord.api.interaction.SlashCommandInteraction
+
+class MarketCommand : Command(
+    "market",
+    "Permet de faire des transactions entre les joueurs",
+    "market",
+    inDms = false
+), ExecutableWithArguments {
+
+    override val fullName: String
+        get() = "market"
+    override val botPerms: Array<String>?
+        get() = arrayOf("PLAY")
+
+    override fun execute(slashCommand: SlashCommandInteraction) {
+        TODO("Not yet implemented")
+    }
 }
