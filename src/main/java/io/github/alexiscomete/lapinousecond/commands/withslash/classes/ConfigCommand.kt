4e7ff002d6fe8d64e6c8fdab16dbbb10203ab640
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-09-05 20:03:50 +0200

    fin configuration de base NORMAL et TUTO

--- src/main/java/io/github/alexiscomete/lapinousecond/commands/withslash/classes/ConfigCommand.kt
+++ src/main/java/io/github/alexiscomete/lapinousecond/commands/withslash/classes/ConfigCommand.kt
@@ -3,11 +3,15 @@ package io.github.alexiscomete.lapinousecond.commands.withslash.classes
 import io.github.alexiscomete.lapinousecond.commands.withslash.Command
 import io.github.alexiscomete.lapinousecond.commands.withslash.ExecutableWithArguments
 import io.github.alexiscomete.lapinousecond.message_event.MenuBuilder
+import io.github.alexiscomete.lapinousecond.modalManager
 import io.github.alexiscomete.lapinousecond.useful.managesave.generateUniqueID
 import io.github.alexiscomete.lapinousecond.worlds.WorldEnum
 import io.github.alexiscomete.lapinousecond.worlds.dibimap.isDibimap
 import io.github.alexiscomete.lapinousecond.worlds.places
 import io.github.alexiscomete.lapinousecond.worlds.servers
+import org.javacord.api.entity.message.component.ActionRow
+import org.javacord.api.entity.message.component.TextInput
+import org.javacord.api.entity.message.component.TextInputStyle
 import org.javacord.api.entity.permission.PermissionType
 import org.javacord.api.interaction.SlashCommandInteraction
 import java.awt.Color
@@ -37,7 +41,7 @@ class ConfigCommand : Command(
                 "Votre serveur discord a été automatiquement assigné au ${world.nameRP}. Explications :\nLe Dibistan a un drapeau qui est aussi son territoire principal. Si votre serveur discord est un état ou une région qui a un territoire en forme de polygone sur le drapeau, alors sont monde est le ${WorldEnum.DIBIMAP.nameRP} sinon c'est le monde ${WorldEnum.NORMAL.nameRP}. Les mécaniques sont différentes dans les 2 mondes. **Le monde détecté est-il correcte ?**",
                 Color.BLUE
             )
-                .addButton("Oui", "Le monde est correcte et je continue la configuration. **Irréversible**") {
+                .addButton("Oui", "Le monde est correcte et je continue la configuration. **Irréversible**") { yes ->
 
                     when (world) {
                         WorldEnum.NORMAL -> {
@@ -45,6 +49,68 @@ class ConfigCommand : Command(
                             val idNameRP = generateUniqueID()
                             val idDescription = generateUniqueID()
                             val idWelcome = generateUniqueID()
+
+                            yes.buttonInteraction.respondWithModal(
+                                id.toString(),
+                                "Configuration de la ville du serveur",
+                                ActionRow.of(
+                                    TextInput.create(
+                                        TextInputStyle.SHORT,
+                                        idNameRP.toString(),
+                                        "Nom de la ville (rp)",
+                                        true
+                                    ),
+                                    TextInput.create(
+                                        TextInputStyle.PARAGRAPH,
+                                        idDescription.toString(),
+                                        "Description de la ville (rp)",
+                                        true
+                                    ),
+                                    TextInput.create(
+                                        TextInputStyle.PARAGRAPH,
+                                        idWelcome.toString(),
+                                        "Message de bienvenue",
+                                        true
+                                    )
+                                )
+                            )
+
+                            modalManager.add(id) {
+                                val opNameRp = it.modalInteraction.getTextInputValueByCustomId(idNameRP.toString())
+                                val opDescription = it.modalInteraction.getTextInputValueByCustomId(idDescription.toString())
+                                val opWelcome = it.modalInteraction.getTextInputValueByCustomId(idWelcome.toString())
+
+                                if (!opNameRp.isPresent || !opDescription.isPresent || !opWelcome.isPresent) {
+                                    throw IllegalArgumentException("Un des champs n'a pas été rempli")
+                                }
+
+                                val placeId = generateUniqueID()
+                                places.add(placeId)
+                                val place = places[placeId]
+                                    ?: throw IllegalArgumentException("Un problème de source inconnue est survenue. La création du serveur a échoué.")
+
+                                place["nameRP"] = opNameRp.get()
+                                place["description"] = opDescription.get()
+                                place["welcome"] = opWelcome.get()
+
+                                // génération aléatoire des coordonnées du lieu entre 1 et le maximum du lieu
+                                var x: Int
+                                var y: Int
+                                do {
+                                    y = (1..world.mapWidth).random()
+                                    x = (1..world.mapHeight).random()
+                                } while (world.isDirt(x, y))
+
+                                place["x"] = x.toString()
+                                place["y"] = y.toString()
+
+                                place["type"] = "city" // automatique normalement
+                                place["world"] = world.progName // automatique normalement
+                                place["server"] = serverId.toString() // automatique normalement
+
+                                configNormalServer(world, slashCommand, placeId)
+
+                            }
                         }
                         WorldEnum.DIBIMAP -> {
                             servers.add(serverId)
@@ -58,21 +124,16 @@ class ConfigCommand : Command(
                             places.add(id)
                             val place = places[id]
                                 ?: throw IllegalArgumentException("Un problème de source inconnue est survenue. La création du serveur a échoué.")
-                            place["nameRP"] = "Saint-Lapin-sur-bot" // TODO
-                            place["description"] = "Ville accueillante du tutoriel"
-                            place["welcome"] = "Ne restez pas trop longtemps ici ! Profitez de l'aventure"
-                            place["x"] = 45.toString()
-                            place["y"] = 20.toString()
-                            place["type"] = "city"
-                            place["world"] = world.progName
-                            place["server"] = serverId.toString()
+                            place["nameRP"] = "Saint-Lapin-sur-bot" // à Demander
+                            place["description"] = "Ville accueillante du tutoriel" // à Demander
+                            place["welcome"] = "Ne restez pas trop longtemps ici ! Profitez de l'aventure" // à Demander
+                            place["x"] = 45.toString() // automatique normalement : aléatoire
+                            place["y"] = 20.toString() // automatique normalement : aléatoire
+                            place["type"] = "city" // automatique normalement
+                            place["world"] = world.progName // automatique normalement
+                            place["server"] = serverId.toString() // automatique normalement
 
-                            servers.add(serverId)
-                            val serverC = servers[serverId]
-                                ?: throw IllegalArgumentException("Un problème de source inconnue est survenue. La création du serveur a échoué.")
-                            serverC["world"] = world.progName
-                            serverC["name"] = slashCommand.server.get().name
-                            serverC["places"] = id.toString()
+                            configNormalServer(world, slashCommand, id)
                         }
                     }
 
@@ -90,4 +151,13 @@ class ConfigCommand : Command(
         }
     }
 
+    private fun configNormalServer(world: WorldEnum, slashCommand: SlashCommandInteraction, placeId: Long) {
+        val server = slashCommand.server.get()
+        servers.add(server.id)
+        val serverC = servers[server.id]
+            ?: throw IllegalArgumentException("Un problème de source inconnue est survenue. La création du serveur a échoué.")
+        serverC["world"] = world.progName
+        serverC["name"] = server.name
+        serverC["places"] = placeId.toString()
+    }
 }
