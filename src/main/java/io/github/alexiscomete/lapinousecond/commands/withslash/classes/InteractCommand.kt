dbf9af8e32e1b8308116cae0462a99e52aede978
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-09-11 16:22:08 +0200

    suite

--- src/main/java/io/github/alexiscomete/lapinousecond/commands/withslash/classes/InteractCommand.kt
+++ src/main/java/io/github/alexiscomete/lapinousecond/commands/withslash/classes/InteractCommand.kt
@@ -3,7 +3,9 @@ package io.github.alexiscomete.lapinousecond.commands.withslash.classes
 import io.github.alexiscomete.lapinousecond.commands.withslash.Command
 import io.github.alexiscomete.lapinousecond.commands.withslash.ExecutableWithArguments
 import io.github.alexiscomete.lapinousecond.commands.withslash.getAccount
+import io.github.alexiscomete.lapinousecond.useful.managesave.saveManager
 import io.github.alexiscomete.lapinousecond.worlds.WorldEnum
+import io.github.alexiscomete.lapinousecond.worlds.places
 import org.javacord.api.entity.message.MessageFlag
 import org.javacord.api.interaction.SlashCommandInteraction
 
@@ -23,7 +25,14 @@ class InteractCommandBase : Command(
         val world = WorldEnum.valueOf(player["world"])
         when (player["place_${world.progName}_type"]) {
             "coos" -> {
-
+                // on regarde si il existe une ville à l'endroit où le joueur est
+                val x = player["place_${world.progName}_x"].toInt()
+                val y = player["place_${world.progName}_y"].toInt()
+                val resultSet = saveManager.executeQuery("SELECT * FROM places WHERE x = $x AND y = $y AND world = '${world.progName}'", true)
+                    ?: throw IllegalArgumentException("Error while executing query")
+                if (resultSet.next()) {
+                    val place = places[resultSet.getString("id").toLong()]
+                }
             }
             "building" -> {
 
