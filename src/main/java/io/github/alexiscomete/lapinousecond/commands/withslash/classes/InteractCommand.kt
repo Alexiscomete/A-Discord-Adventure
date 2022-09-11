4c6bacaabc5bb96096c05c44cc4558596d57911e
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-09-11 16:15:07 +0200

    début intéractions

--- src/main/java/io/github/alexiscomete/lapinousecond/commands/withslash/classes/InteractCommand.kt
+++ src/main/java/io/github/alexiscomete/lapinousecond/commands/withslash/classes/InteractCommand.kt
@@ -1,3 +1,43 @@
 package io.github.alexiscomete.lapinousecond.commands.withslash.classes
 
-//TOTO
+import io.github.alexiscomete.lapinousecond.commands.withslash.Command
+import io.github.alexiscomete.lapinousecond.commands.withslash.ExecutableWithArguments
+import io.github.alexiscomete.lapinousecond.commands.withslash.getAccount
+import io.github.alexiscomete.lapinousecond.worlds.WorldEnum
+import org.javacord.api.entity.message.MessageFlag
+import org.javacord.api.interaction.SlashCommandInteraction
+
+class InteractCommandBase : Command(
+    "interact",
+    "Interact with your environment",
+    "interact"
+), ExecutableWithArguments {
+
+    override val fullName: String
+        get() = "interact"
+    override val botPerms: Array<String>
+        get() = arrayOf("PLAY")
+
+    override fun execute(slashCommand: SlashCommandInteraction) {
+        val player = getAccount(slashCommand)
+        val world = WorldEnum.valueOf(player["world"])
+        when (player["place_${world.progName}_type"]) {
+            "coos" -> {
+
+            }
+            "building" -> {
+
+            }
+            "city" -> {
+
+            }
+            else -> {
+                slashCommand.createImmediateResponder()
+                    .setContent("Aucune interaction possible ici")
+                    .setFlags(MessageFlag.EPHEMERAL)
+                    .respond()
+            }
+        }
+    }
+
+}
