91999b61961f2666d6d3c42438010ee814d68e5a
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-10-26 19:52:01 +0200

    début context + dialogues

--- src/main/java/io/github/alexiscomete/lapinousecond/message_event/Context.kt
+++ src/main/java/io/github/alexiscomete/lapinousecond/message_event/Context.kt
@@ -2,6 +2,37 @@ package io.github.alexiscomete.lapinousecond.message_event
 
 import io.github.alexiscomete.lapinousecond.entity.Player
 
-class Context(val player: Player, val otherPlayers: List<Player> = listOf()) {
+data class Players(val player: Player, val otherPlayers: List<Player> = listOf())
+
+val contexts = mutableMapOf<Players, Context>()
+
+fun contextForOrNull(player: Player, otherPlayers: List<Player>): Context? {
+    return contexts[Players(player, otherPlayers)]
+}
+
+fun contextFor(player: Player, otherPlayers: List<Player>): Context {
+    return contextForOrNull(player, otherPlayers)
+        ?: run {
+            val players = Players(player, otherPlayers)
+            Context(players)
+                .also {
+                    contexts[players] = it
+                }
+        }
+}
+
+fun contextFor(players: List<Player>): Context {
+    // for each combination of players
+    for (player in players) {
+        players.minus(player).also { list ->
+            if (list.isNotEmpty()) {
+                contextForOrNull(player, list)?.let { return it }
+            }
+        }
+    }
+    return contextFor(players.first(), players.minus(players.first()))
+}
+
+class Context(val players: Players) {
 
 }
