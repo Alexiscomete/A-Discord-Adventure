6818aab3dac9cce0a58e489fbb457975bfa86237
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-10-29 13:18:19 +0200

    suite context

--- src/main/java/io/github/alexiscomete/lapinousecond/view/Context.kt
+++ src/main/java/io/github/alexiscomete/lapinousecond/view/Context.kt
@@ -35,9 +35,54 @@ fun contextFor(players: List<PlayerWithAccount>): Context {
     return contextFor(players.first(), players.minus(players.first()))
 }
 
-class Context(val players: Players?) {
-    val buttons: ButtonsContextManager? = null
-    val selectMenu: ((SelectMenuChooseEvent, Context) -> Unit)? = null
-    val multiContext: Context? = null
+class Context(val players: Players?, canParallel: Boolean = false) {
 
+    init {
+        if (players != null) {
+            if (players.otherPlayers.isNotEmpty()) {
+                for (player in players.otherPlayers) {
+                    contextFor(player).also {
+                        it.multiContext(this, canParallel)
+                    }
+                }
+            }
+        }
+    }
+
+    var buttons: ButtonsContextManager? = null
+        private set
+    var selectMenu: ((SelectMenuChooseEvent, Context) -> Unit)? = null
+        private set
+    var multiContext: Context? = null
+        private set
+
+    fun buttons(buttons: ButtonsContextManager, canParallel:Boolean=false): Context {
+        if (!canParallel) {
+            clear()
+        }
+        this.buttons = buttons
+        return this
+    }
+
+    fun selectMenu(selectMenu: (SelectMenuChooseEvent, Context) -> Unit, canParallel:Boolean=false): Context {
+        if (!canParallel) {
+            clear()
+        }
+        this.selectMenu = selectMenu
+        return this
+    }
+
+    fun multiContext(multiContext: Context, canParallel:Boolean=false): Context {
+        if (!canParallel) {
+            clear()
+        }
+        this.multiContext = multiContext
+        return this
+    }
+
+    fun clear() {
+        buttons = null
+        selectMenu = null
+        multiContext = null
+    }
 }
