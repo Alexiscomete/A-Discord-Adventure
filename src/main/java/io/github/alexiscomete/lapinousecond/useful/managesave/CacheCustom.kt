650df2ffef0102cf7b8bcc0f2c2592c07d296edd
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-10-30 19:57:46 +0100

    suite context

--- src/main/java/io/github/alexiscomete/lapinousecond/useful/managesave/CacheCustom.kt
+++ src/main/java/io/github/alexiscomete/lapinousecond/useful/managesave/CacheCustom.kt
@@ -2,7 +2,7 @@ package io.github.alexiscomete.lapinousecond.useful.managesave
 
 import java.sql.SQLException
 
-class CacheCustom<U>(private val table: Table, private val function: (Long) -> U) {
+open class CacheCustom<U>(private val table: Table, protected val function: (Long) -> U) {
     val hashMap = HashMap<Long, U>()
     operator fun get(l: Long): U? {
         var u = hashMap[l]
@@ -27,5 +27,28 @@ class CacheCustom<U>(private val table: Table, private val function: (Long) -> U
         val hashMap = HashMap<String, String>()
         hashMap["id"] = id.toString()
         saveManager.insert(table.name, hashMap)
+    }
+}
+
+class CacheCustomWithArgument<U, V, W>(private val table: Table, private val function2: (Long, V) -> U, ) {
+    private val hashMap2 = HashMap<Long, U>()
+
+    operator fun get(l: Long, v: V): U? {
+        var u = hashMap2[l]
+        if (u == null) {
+            try {
+                val resultSet = saveManager.executeQuery("SELECT * FROM " + table.name + " WHERE id = " + l, true)
+                if (resultSet != null) {
+                    if (resultSet.next()) {
+                        u = function2(l, v)
+                        hashMap2[l] = u
+                    }
+                }
+                resultSet?.close()
+            } catch (throwables: SQLException) {
+                throwables.printStackTrace()
+            }
+        }
+        return u
     }
 }
