53d7c38f7f8ddb83ad8c011ce2f20d5ff41c267e
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-11-23 20:00:38 +0100

    PlayerUI interface

---
+++ src/main/java/io/github/alexiscomete/lapinousecond/view/ui/InteractionUI.kt
@@ -0,0 +1,5 @@
+package io.github.alexiscomete.lapinousecond.view.ui
+
+interface InteractionUI {
+    fun execute(ui: PlayerUI): InteractionUI
+}
--- src/main/java/io/github/alexiscomete/lapinousecond/view/ui/PlayerUI.kt
+++ src/main/java/io/github/alexiscomete/lapinousecond/view/ui/PlayerUI.kt
@@ -1,4 +1,25 @@
 package io.github.alexiscomete.lapinousecond.view.ui
 
-class PlayerUI {
+import java.awt.image.BufferedImage
+
+interface PlayerUI {
+    fun addMessage(message: String): PlayerUI
+    fun addDialogue(dialogue: Dialogue): PlayerUI
+    fun addInteraction(id: String, interaction: InteractionUI): PlayerUI
+    fun respondToInteraction(id: String): PlayerUI
+    fun respondToInteractionWithArgument(id: String, argument: String): PlayerUI
+    fun removeInteraction(id: String): PlayerUI
+    fun clear(): PlayerUI
+    fun updateOrSend(): PlayerUI
+    fun setImage(image: BufferedImage): PlayerUI
+    fun setImage(link: String): PlayerUI
+    fun hasInteraction(id: String): Boolean
+    fun hasDialogue(): Boolean
+    fun hasMessage(): Boolean
+    fun hasImage(): Boolean
+    fun getInteraction(id: String): InteractionUI?
+    fun getImage(): BufferedImage?
+    fun getInteractions(): Map<String, InteractionUI>
+    fun getMessages(): List<String>
+    fun getDialogues(): List<Dialogue>
 }
