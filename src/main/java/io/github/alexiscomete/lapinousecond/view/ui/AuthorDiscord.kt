23634733484c941ff9fc32232e77d7d5ff1ae87a
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-11-25 18:29:13 +0100

    author

--- src/main/java/io/github/alexiscomete/lapinousecond/view/ui/AuthorDiscord.kt
+++ src/main/java/io/github/alexiscomete/lapinousecond/view/ui/AuthorDiscord.kt
@@ -1,4 +1,25 @@
 package io.github.alexiscomete.lapinousecond.view.ui
 
-class AuthorDiscord {
+import org.javacord.api.entity.user.User
+
+class AuthorDiscord(user: User, name: String? = null, avatar: String? = null, image: String? = null) : Author {
+    override fun getName(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun getAvatar(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun hasAvatar(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun setName(name: String): Author {
+        TODO("Not yet implemented")
+    }
+
+    override fun setAvatar(avatar: String): Author {
+        TODO("Not yet implemented")
+    }
 }
