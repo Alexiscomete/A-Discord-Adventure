cc885fee7e4e783afadd6b24f88b5557898a7b5a
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-11-25 18:41:56 +0100

    Discord author ✅

--- src/main/java/io/github/alexiscomete/lapinousecond/view/ui/Author.kt
+++ src/main/java/io/github/alexiscomete/lapinousecond/view/ui/Author.kt
@@ -1,10 +1,14 @@
 package io.github.alexiscomete.lapinousecond.view.ui
 
+import java.awt.image.BufferedImage
+
 interface Author {
     fun getName(): String
-    fun getAvatar(): String
-    fun hasAvatar(): Boolean
+    fun getLinkAvatar(): String?
+    fun hasLinkAvatar(): Boolean
     fun setName(name: String): Author
     fun setAvatar(avatar: String): Author
-
+    fun getImageAvatar(): BufferedImage?
+    fun setAvatar(avatar: BufferedImage): Author
+    fun hasImageAvatar(): Boolean
 }
--- src/main/java/io/github/alexiscomete/lapinousecond/view/ui/AuthorDiscord.kt
+++ src/main/java/io/github/alexiscomete/lapinousecond/view/ui/AuthorDiscord.kt
@@ -1,25 +1,58 @@
 package io.github.alexiscomete.lapinousecond.view.ui
 
 import org.javacord.api.entity.user.User
+import java.awt.image.BufferedImage
 
-class AuthorDiscord(user: User, name: String? = null, avatar: String? = null, image: String? = null) : Author {
+class AuthorDiscord(user: User, name: String? = null, avatar: String? = null, image: BufferedImage? = null) : Author {
+
+    private var playerName: String
+    private var playerAvatar: String? = null
+    private var playerImage: BufferedImage? = null
+
+    init {
+        playerName = name ?: user.name
+        if (image != null) {
+            playerImage = image
+        } else if (avatar != null) {
+            playerAvatar = avatar
+        } else if (user.avatar != null) {
+            playerAvatar = user.avatar.url.toString()
+        }
+    }
+
     override fun getName(): String {
-        TODO("Not yet implemented")
+        return playerName
     }
 
-    override fun getAvatar(): String {
-        TODO("Not yet implemented")
+    override fun getLinkAvatar(): String {
+        return playerAvatar ?: throw IllegalStateException("No avatar link")
     }
 
-    override fun hasAvatar(): Boolean {
-        TODO("Not yet implemented")
+    override fun hasLinkAvatar(): Boolean {
+        return playerAvatar != null
     }
 
     override fun setName(name: String): Author {
-        TODO("Not yet implemented")
+        playerName = name
+        return this
     }
 
     override fun setAvatar(avatar: String): Author {
-        TODO("Not yet implemented")
+        playerAvatar = avatar
+        return this
     }
+
+    override fun setAvatar(avatar: BufferedImage): Author {
+        playerImage = avatar
+        return this
+    }
+
+    override fun getImageAvatar(): BufferedImage {
+        return playerImage ?: throw IllegalStateException("No avatar image")
+    }
+
+    override fun hasImageAvatar(): Boolean {
+        return playerImage != null
+    }
+
 }
