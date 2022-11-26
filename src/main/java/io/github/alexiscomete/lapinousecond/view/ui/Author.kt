c9a61d8240665158dd13f66f96605a80ae158fef
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-11-26 11:22:04 +0100

    author

--- src/main/java/io/github/alexiscomete/lapinousecond/view/ui/Author.kt
+++ src/main/java/io/github/alexiscomete/lapinousecond/view/ui/Author.kt
@@ -4,11 +4,11 @@ import java.awt.image.BufferedImage
 
 interface Author {
     fun getName(): String
-    fun getLinkAvatar(): String?
+    fun getLinkAvatar(): String
     fun hasLinkAvatar(): Boolean
     fun setName(name: String): Author
     fun setAvatar(avatar: String): Author
-    fun getImageAvatar(): BufferedImage?
+    fun getImageAvatar(): BufferedImage
     fun setAvatar(avatar: BufferedImage): Author
     fun hasImageAvatar(): Boolean
 }
--- src/main/java/io/github/alexiscomete/lapinousecond/view/ui/AuthorFiction.kt
+++ src/main/java/io/github/alexiscomete/lapinousecond/view/ui/AuthorFiction.kt
@@ -1,4 +1,41 @@
 package io.github.alexiscomete.lapinousecond.view.ui
 
-class AuthorFiction {
+import org.javacord.api.entity.user.User
+import java.awt.image.BufferedImage
+
+class AuthorFiction(user: User, private var playerName: String, var avatar: String? = null, var image: BufferedImage? = null) : Author {
+    override fun getName(): String {
+        return playerName
+    }
+
+    override fun getLinkAvatar(): String {
+        return avatar ?: throw IllegalStateException("No avatar link")
+    }
+
+    override fun hasLinkAvatar(): Boolean {
+        return avatar != null
+    }
+
+    override fun setName(name: String): Author {
+        playerName = name
+        return this
+    }
+
+    override fun setAvatar(avatar: String): Author {
+        this.avatar = avatar
+        return this
+    }
+
+    override fun setAvatar(avatar: BufferedImage): Author {
+        image = avatar
+        return this
+    }
+
+    override fun getImageAvatar(): BufferedImage {
+        return image ?: throw IllegalStateException("No avatar image")
+    }
+
+    override fun hasImageAvatar(): Boolean {
+        return image != null
+    }
 }
