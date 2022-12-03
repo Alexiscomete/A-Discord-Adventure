c454f0112d0655d484d179c35e272889b2b7b894
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-12-03 13:03:30 +0100

    messages

--- src/main/java/io/github/alexiscomete/lapinousecond/view/ui/DialogueReader.kt
+++ src/main/java/io/github/alexiscomete/lapinousecond/view/ui/DialogueReader.kt
@@ -1,4 +1,10 @@
 package io.github.alexiscomete.lapinousecond.view.ui
 
-class DialogueReader {
+class DialogueReader(
+    private val dialogue: Dialogue,
+    private val ui: PlayerUI,
+    private val onEnd: () -> Unit,
+    private val maxCharOnOnePart: Int = -1
+) {
+
 }
