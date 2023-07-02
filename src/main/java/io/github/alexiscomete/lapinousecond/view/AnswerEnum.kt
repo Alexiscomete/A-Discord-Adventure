a5457011d3e898e788340b2b07e6803a06beb5b2
Alexiscomete <wallan3100@gmail.com>
2023-07-02 12:33:34 +0200

    Langues ?

--- src/main/java/io/github/alexiscomete/lapinousecond/view/AnswerEnum.kt
+++ src/main/java/io/github/alexiscomete/lapinousecond/view/AnswerEnum.kt
@@ -2,23 +2,23 @@ package io.github.alexiscomete.lapinousecond.view
 
 val answerManager = AnswerManager(AnswerEnum::class.java.classLoader.getResourceAsStream("answers.json"))
 
-enum class AnswerEnum(val value: String) {
-    NAME("name"),
-    DESCR("description"),
-    PROGRESSION("progression"),
-    OWNER("owner"),
-    BUILDING_BA("building_ba"),
-    ECHEC_TRANS("echec_trans"),
-    NO_ENOUGH_MONEY("no_enough_money"),
-    CONFIRMATION("confirmation"),
-    MONTANT_TR("montant_tr"),
-    TR_END("tr_end"),
-    ASK_MONTANT("ask_montant"),
-    FORM_INVALID("form_invalid"),
-    VALUE_TOO_HIGH("value_too_high"),
-    IMP_SIT("imp_sit"),
-    NO_ENOUGH_ARGS("no_enough_args"),
-    ILLEGAL_ARGUMENT_NUMBER("illegal_argument_number"),
-    ENTREE_BUILD("entree_build"),
-    LIST_BUILDINGS("list_buildings");
+enum class AnswerEnum {
+    NAME,
+    DESCR,
+    PROGRESSION,
+    OWNER,
+    BUILDING_BA,
+    ECHEC_TRANS,
+    NO_ENOUGH_MONEY,
+    CONFIRMATION,
+    MONTANT_TR,
+    TR_END,
+    ASK_MONTANT,
+    FORM_INVALID,
+    VALUE_TOO_HIGH,
+    IMP_SIT,
+    NO_ENOUGH_ARGS,
+    ILLEGAL_ARGUMENT_NUMBER,
+    ENTREE_BUILD,
+    LIST_BUILDINGS;
 }
