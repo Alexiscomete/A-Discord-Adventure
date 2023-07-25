b6ed170ffc78fd7971d61225d9280ac4e7330d39
Alexiscomete <wallan3100@gmail.com>
2023-07-25 12:48:52 +0200

    Feat: Starting a monster.

--- src/main/java/io/github/alexiscomete/lapinousecond/worlds/map/tiles/sprite/InteractionSpriteManager.kt
+++
@@ -1,8 +0,0 @@
-package io.github.alexiscomete.lapinousecond.worlds.map.tiles.sprite
-
-import io.github.alexiscomete.lapinousecond.worlds.map.tiles.sprite.interactions.InteractionSprite
-
-interface InteractionSpriteManager {
-    fun getAllElements(): List<InteractionSprite>
-    fun updateAfter()
-}
--- src/main/java/io/github/alexiscomete/lapinousecond/worlds/map/tiles/textures/ImagesOperations.kt
+++ src/main/java/io/github/alexiscomete/lapinousecond/worlds/map/tiles/textures/ImagesOperations.kt
@@ -4,10 +4,25 @@ import java.awt.geom.AffineTransform
 import java.awt.image.AffineTransformOp
 import java.awt.image.BufferedImage
 
+/**
+ * Mirror the image horizontally. WARNING: this method is executed every time it is called.
+ */
 val BufferedImage.mirrorImage: BufferedImage
     get() {
         val transform = AffineTransform.getScaleInstance(-1.0, 1.0) // Miroir horizontal
         transform.translate(-width.toDouble(), 0.0) // Ajustement des coordonnées
+
+        val op = AffineTransformOp(transform, AffineTransformOp.TYPE_NEAREST_NEIGHBOR)
+        return op.filter(this, null)
+    }
+
+/**
+ * WARNING: this method is executed every time it is called.
+ */
+val BufferedImage.mirrorImageVertically: BufferedImage
+    get() {
+        val transform = AffineTransform.getScaleInstance(1.0, -1.0) // Miroir vertical
+        transform.translate(0.0, -height.toDouble()) // Ajustement des coordonnées
 
         val op = AffineTransformOp(transform, AffineTransformOp.TYPE_NEAREST_NEIGHBOR)
         return op.filter(this, null)
