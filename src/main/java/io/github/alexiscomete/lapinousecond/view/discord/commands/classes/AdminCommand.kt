482dc0cef1163142ee7442d2bfdcc18692e5c74b
Alexiscomete <wallan3100@gmail.com>
2023-08-11 12:29:06 +0200

    Feat: start AdminCommand.kt

---
+++ src/main/java/io/github/alexiscomete/lapinousecond/view/discord/commands/classes/AdminCommand.kt
@@ -0,0 +1,15 @@
+package io.github.alexiscomete.lapinousecond.view.discord.commands.classes
+
+class AdminCommandBase {
+
+}
+
+class AdminCommandExecuteSQL {
+}
+
+class AdminCommandQuerySQL {
+}
+
+class AdminCommandCache {
+
+}
